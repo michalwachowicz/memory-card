027fbeddf79e42ead15e746a265271e2a41796a2
michalwachowicz <wachowiczmichal1@gmail.com>
2024-11-28 21:10:45 +0100

    Use state for help button

--- src/components/buttons/HelpButton.tsx
+++ src/components/buttons/HelpButton.tsx
@@ -1,27 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import HelpIcon from "@/Assets/images/icons/help.svg?react";
 import CloseIcon from "@/Assets/images/icons/close.svg?react";
 import HelpMenu from "@/Components/HelpMenu";
 
-interface Props {
-  open?: boolean;
-  onToggle?: (open?: boolean) => void;
-}
+const HelpButton: React.FC = () => {
+  const [open, setOpen] = useState(false);
 
-const HelpButton: React.FC<Props> = ({ open = false, onToggle = () => {} }) => (
-  <div className="btn-help-container">
-    {open && <HelpMenu />}
-    <button
-      className="btn btn-icon btn-help"
-      type="button"
-      role="switch"
-      aria-checked={open}
-      aria-label={`${open ? "Close" : "Open"} help menu`}
-      onClick={() => onToggle && onToggle(!open)}
-    >
-      <div className="btn-inner">{open ? <CloseIcon /> : <HelpIcon />}</div>
-    </button>
-  </div>
-);
+  return (
+    <div className="btn-help-container">
+      {open && <HelpMenu />}
+      <button
+        className="btn btn-icon btn-help"
+        type="button"
+        role="switch"
+        aria-checked={open}
+        aria-label={`${open ? "Close" : "Open"} help menu`}
+        onClick={() => setOpen(!open)}
+      >
+        <div className="btn-inner">{open ? <CloseIcon /> : <HelpIcon />}</div>
+      </button>
+    </div>
+  );
+};
 
 export default HelpButton;
