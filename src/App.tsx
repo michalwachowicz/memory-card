3109459410ad627f5a260fd61f961d3c913ebcb0
michalwachowicz <wachowiczmichal1@gmail.com>
2024-11-28 21:39:34 +0100

    Add buttons, main screen, and bg to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Background from "@/Components/Background";
+import MainScreen from "@/Components/screens/MainScreen";
+import SettingsButtons from "@/Components/buttons/SettingsButtons";
 
-const App = () => (
-  <div>
-    <Background opacity={0.55} music={false} />
-  </div>
-);
+const App = () => {
+  const [sound, setSound] = useState(false);
+
+  const toggleSound = (newSound: boolean | undefined) => {
+    if (newSound === undefined) return;
+    setSound(newSound);
+  };
+
+  return (
+    <div>
+      <SettingsButtons soundOn={sound} onSoundToggle={toggleSound} />
+      <MainScreen />
+      <Background opacity={0.55} music={sound} />
+    </div>
+  );
+};
 
 export default App;
