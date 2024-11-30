fd16f9687a6c38a6d7736a617794dab6bb054e8f
michalwachowicz <wachowiczmichal1@gmail.com>
2024-11-30 14:49:53 +0100

    Add difficulty select prop to main screen

--- src/components/screens/MainScreen.tsx
+++ src/components/screens/MainScreen.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import GameButton from "@/Components/buttons/GameButton";
 import Logo from "@/Assets/images/logo/logo.svg?react";
+import Difficulty from "../../types/Difficulty";
 
-const MainScreen: React.FC = () => (
+interface Props {
+  onDifficultySelect?: (difficulty?: Difficulty) => void;
+}
+
+const MainScreen: React.FC<Props> = ({ onDifficultySelect = () => {} }) => (
   <div className="main">
     <Logo className="main-logo" aria-label="The Witcher Logo" />
     <div className="main-btns">
-      <GameButton label="Easy" difficulty="easy" />
-      <GameButton label="Medium" difficulty="medium" />
-      <GameButton label="Hard" difficulty="hard" />
+      <GameButton label="Easy" difficulty="easy" onClick={onDifficultySelect} />
+      <GameButton
+        label="Medium"
+        difficulty="medium"
+        onClick={onDifficultySelect}
+      />
+      <GameButton label="Hard" difficulty="hard" onClick={onDifficultySelect} />
     </div>
   </div>
 );
--- src/components/screens/__tests__/MainScreen.test.tsx
+++ src/components/screens/__tests__/MainScreen.test.tsx
@@ -1,5 +1,6 @@
-import { describe, expect } from "vitest";
+import { describe, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import MainScreen from "@/Components/screens/MainScreen";
 
 describe("<MainScreen />", () => {
@@ -14,5 +15,16 @@ describe("<MainScreen />", () => {
     [/easy/i, /medium/i, /hard/i].forEach((label) =>
       expect(screen.getByRole("button", { name: label })).toBeInTheDocument(),
     );
+  });
+
+  it("handles click properly", async () => {
+    const fn = vi.fn();
+    render(<MainScreen onDifficultySelect={fn} />);
+
+    const user = userEvent.setup();
+    await user.click(screen.getByRole("button", { name: /easy/i }));
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith("easy");
   });
 });
