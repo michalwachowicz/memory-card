087449261eb6304bd332973b7ac058beec558d5e
michalwachowicz <wachowiczmichal1@gmail.com>
2024-11-30 13:52:14 +0100

    Implement gameplay screen

---
+++ src/components/screens/GameplayScreen.tsx
@@ -0,0 +1,120 @@
+/* eslint-disable no-nested-ternary */
+import React, { useEffect, useState } from "react";
+import LogoButton from "@/Components/buttons/LogoButton";
+import Scoreboard from "@/Components/Scoreboard";
+import CardContainer from "@/Components/CardContainer";
+import { getNextRoundCards } from "../../managers/cardManager";
+import Difficulty from "../../types/Difficulty";
+import Card from "../../types/Card";
+
+interface Props {
+  initialHighScore?: number;
+  difficulty: Difficulty;
+  onGameOver?: (win?: boolean) => void;
+}
+
+const GameplayScreen: React.FC<Props> = ({
+  initialHighScore = 0,
+  difficulty,
+  onGameOver = () => {},
+}) => {
+  const [flipped, setFlipped] = useState(true);
+
+  const [clickedCards, setClickedCards] = useState<number[]>([]);
+  const [cards, setCards] = useState<Card[]>([]);
+  const [cardsSize, setCardsSize] = useState(0);
+
+  const [round, setRound] = useState(1);
+  const [maxRounds, setMaxRounds] = useState(0);
+
+  const [highScore, setHighScore] = useState(initialHighScore);
+  const [score, setScore] = useState(0);
+
+  const reset = () => {
+    setMaxRounds(0);
+    setRound(1);
+    setScore(0);
+    setCardsSize(0);
+    setCards([]);
+    setClickedCards([]);
+  };
+
+  const gameOver = (win: boolean) => {
+    reset();
+    onGameOver(win);
+  };
+
+  const updateHighScore = (newScore: number) => {
+    setHighScore(newScore);
+    localStorage.setItem("highScore", `${newScore}`);
+  };
+
+  const handleCardClick = (id?: number) => {
+    if (id === undefined) return;
+    setFlipped(true);
+
+    if (clickedCards.includes(id)) {
+      gameOver(false);
+      return;
+    }
+
+    const newScore = score + 1;
+    setScore(newScore);
+
+    if (newScore > highScore) {
+      updateHighScore(newScore);
+    }
+
+    if (round >= maxRounds) {
+      gameOver(true);
+      return;
+    }
+
+    const newClickedCards = [...clickedCards, id];
+
+    setClickedCards(newClickedCards);
+    setCards(getNextRoundCards(newClickedCards, cardsSize));
+    setRound((prevRound) => prevRound + 1);
+  };
+
+  useEffect(() => {
+    const newCardsSize =
+      difficulty === "hard" ? 8 : difficulty === "medium" ? 6 : 4;
+
+    setCardsSize(newCardsSize);
+    setCards(getNextRoundCards([], newCardsSize));
+
+    setMaxRounds(difficulty === "hard" ? 10 : difficulty === "medium" ? 8 : 6);
+  }, [difficulty]);
+
+  useEffect(() => {
+    if (!flipped) return () => {};
+
+    const timeout = setTimeout(() => {
+      setFlipped(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [flipped, round]);
+
+  return (
+    <div className="gameplay">
+      <header className="gameplay-header">
+        <LogoButton />
+        <Scoreboard score={score} highScore={highScore} />
+      </header>
+      <div className="gameplay-main">
+        <CardContainer
+          cards={cards}
+          flipped={flipped}
+          onClick={handleCardClick}
+        />
+        <div className="gameplay-round">
+          {round} / {maxRounds}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameplayScreen;
---
+++ src/components/screens/__tests__/GameplayScreen.test.tsx
@@ -0,0 +1,184 @@
+import { afterEach, beforeEach, describe, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { act } from "react";
+import { userEvent } from "@testing-library/user-event";
+import GameplayScreen from "@/Components/screens/GameplayScreen";
+import Card from "../../../types/Card";
+
+vi.mock("@/Components/CardContainer", () => ({
+  default: ({
+    cards,
+    flipped,
+    onClick,
+  }: {
+    cards: Card[];
+    flipped: boolean;
+    onClick: (id?: number) => void;
+  }) => (
+    <div data-testid="cards">
+      {cards.map((card) => (
+        <button
+          type="button"
+          onClick={() => onClick(card.id)}
+          key={card.id}
+          data-testid="card"
+          data-id={card.id}
+        >
+          {flipped ? "flipped" : card.name}
+        </button>
+      ))}
+    </div>
+  ),
+}));
+
+describe("<GameplayScreen />", () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("renders with proper initial values", () => {
+    const { rerender } = render(
+      <GameplayScreen difficulty="easy" initialHighScore={4} />,
+    );
+
+    expect(screen.getByText(/high score: 4/i)).toBeInTheDocument();
+    expect(screen.getByText(/^score: 0/i)).toBeInTheDocument();
+    expect(screen.getByText(/1 \/ 6/i)).toBeInTheDocument();
+
+    rerender(<GameplayScreen difficulty="medium" initialHighScore={2} />);
+
+    expect(screen.getByText(/high score: 4/i)).toBeInTheDocument();
+    expect(screen.getByText(/^score: 0/i)).toBeInTheDocument();
+    expect(screen.getByText(/1 \/ 8/i)).toBeInTheDocument();
+  });
+
+  it("increases values on card click", async () => {
+    render(<GameplayScreen difficulty="easy" />);
+    expect(screen.getByText(/high score: 0/i)).toBeInTheDocument();
+    expect(screen.getByText(/^score: 0/i)).toBeInTheDocument();
+    expect(screen.getByText(/1 \/ 6/i)).toBeInTheDocument();
+
+    const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
+    await act(async () => {
+      await user.click(screen.getAllByTestId("card")[0]);
+    });
+
+    expect(screen.getByText(/high score: 1/i)).toBeInTheDocument();
+    expect(screen.getByText(/^score: 1/i)).toBeInTheDocument();
+    expect(screen.getByText(/2 \/ 6/i)).toBeInTheDocument();
+  });
+
+  it("increases only score if high score is bigger", async () => {
+    render(<GameplayScreen difficulty="easy" initialHighScore={2} />);
+    expect(screen.getByText(/high score: 2/i)).toBeInTheDocument();
+    expect(screen.getByText(/^score: 0/i)).toBeInTheDocument();
+    expect(screen.getByText(/1 \/ 6/i)).toBeInTheDocument();
+
+    const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
+    await act(async () => {
+      await user.click(screen.getAllByTestId("card")[0]);
+    });
+
+    expect(screen.getByText(/high score: 2/i)).toBeInTheDocument();
+    expect(screen.getByText(/^score: 1/i)).toBeInTheDocument();
+    expect(screen.getByText(/2 \/ 6/i)).toBeInTheDocument();
+  });
+
+  it("flips the cards on click", async () => {
+    render(<GameplayScreen difficulty="easy" />);
+
+    const checkCards = async () => {
+      screen.getAllByTestId("card").forEach((card) => {
+        expect(card).toHaveAccessibleName("flipped");
+      });
+      await act(() => vi.advanceTimersByTime(1000));
+      screen.getAllByTestId("card").forEach((card) => {
+        expect(card).not.toHaveAccessibleName("flipped");
+      });
+    };
+    await checkCards();
+
+    const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
+    await act(async () => {
+      await user.click(screen.getAllByTestId("card")[0]);
+    });
+
+    await checkCards();
+  });
+
+  it("handles winning the game properly", async () => {
+    const clickedCards: number[] = [];
+    const fn = vi.fn();
+
+    render(<GameplayScreen difficulty="easy" onGameOver={fn} />);
+    await act(() => vi.advanceTimersByTime(2000));
+
+    const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
+
+    const handleClick = async () => {
+      const cards = screen.getAllByTestId("card");
+      const unclickedCard = cards.find(
+        (card) =>
+          card.textContent &&
+          !clickedCards.includes(parseInt(card.dataset.id || "-1", 10)),
+      );
+
+      if (unclickedCard) {
+        await user.click(unclickedCard);
+        clickedCards.push(parseInt(unclickedCard.dataset.id || "-1", 10));
+      }
+    };
+
+    for (let i = 0; i < 6; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      await act(async () => {
+        await handleClick();
+        vi.advanceTimersByTime(1000);
+      });
+    }
+
+    expect(clickedCards).not.toContain(-1);
+    expect(clickedCards).toHaveLength(6);
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith(true);
+  });
+
+  it("handles losing the game properly", async () => {
+    const clickedCards: number[] = [];
+    const fn = vi.fn();
+
+    render(<GameplayScreen difficulty="easy" onGameOver={fn} />);
+    await act(() => vi.advanceTimersByTime(1000));
+
+    const user = userEvent.setup({ advanceTimers: vi.advanceTimersByTime });
+
+    const handleClick = async () => {
+      const cards = screen.getAllByTestId("card");
+      const clickedCard =
+        cards.find(
+          (card) =>
+            card.textContent &&
+            clickedCards.includes(parseInt(card.dataset.id || "-1", 10)),
+        ) || cards[0];
+
+      await user.click(clickedCard);
+      clickedCards.push(parseInt(clickedCard.dataset.id || "-1", 10));
+    };
+
+    while (screen.queryAllByTestId("card").length > 0) {
+      // eslint-disable-next-line no-await-in-loop
+      await act(async () => {
+        await handleClick();
+        vi.advanceTimersByTime(1000);
+      });
+    }
+
+    expect(fn).toHaveBeenCalledTimes(1);
+    expect(fn).toHaveBeenCalledWith(false);
+  });
+});
