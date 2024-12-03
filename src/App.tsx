dd30d0dadd107b663f1de7250fa07e5e6035cbf4
michalwachowicz <wachowiczmichal1@gmail.com>
2024-12-03 14:08:48 +0100

    Add loading screen to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "@/Components/Background";
 import GameWrapper from "@/Components/GameWrapper";
+import LoadingScreen from "@/Components/screens/LoadingScreen";
+import { preloadCards } from "./managers/cardManager";
 
 const App = () => {
   const [sound, setSound] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const toggleSound = (newSound: boolean | undefined) => {
     if (newSound === undefined) return;
     setSound(newSound);
   };
 
+  useEffect(() => {
+    preloadCards();
+
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div>
-      <GameWrapper sound={sound} onSoundToggle={toggleSound} />
-      <Background opacity={0.55} music={sound} />
+      {loading && <LoadingScreen />}
+      {!loading && (
+        <GameWrapper sound={sound} onSoundToggle={toggleSound} />
+      )};
+      <Background opacity={loading ? 0.9 : 0.55} music={sound} />
     </div>
   );
 };
